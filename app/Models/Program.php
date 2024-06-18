4b5a8aedb106f7314794e4503c2505c50f3b298f
maricor <m.masong14@gmail.com>
2024-06-18 10:49:30 +0800

    created models for each table

---
+++ app/Models/Program.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    protected $primaryKey = 'progid';
+    protected $fillable = ['progfullname', 'progshortname', 'progcollid', 'progcolldeptid'];
+
+    public function college()
+    {
+        return $this->belongsTo(College::class, 'progcollid');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'progcolldeptid');
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'studprogid');
+    }
+}
