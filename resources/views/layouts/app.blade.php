557fc1bdad3e8ea0ab035be6d7117324f9f014e7
maricor <m.masong14@gmail.com>
2024-06-19 09:39:10 +0800

    show students, colleges and depts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/show/colleges') }}">All Colleges</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/show/colleges') }}">All Departments</a>
+                </li>
             </ul>
         </div>
     </nav>
